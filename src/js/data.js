ac31e50125508cf1e09c419a6872e73293fbd6f4
Ian Wessen <ian@honeybook.com>
2018-07-08 18:24:39 -0700

    initial commit

---
+++ src/js/data.js
@@ -0,0 +1,15 @@
+
+// export default const sum = (a,b) => a + b
+// const Info = () => {
+
+//     const _navPoints {
+//         return '6'
+//     }
+
+//     return {
+//         _navPoints: _navPoints
+//     }
+
+// }
+
+// export default { Info };
